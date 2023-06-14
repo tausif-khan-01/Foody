319ccbb09862079b35dc1f793c8252a6aaffa5cb
Tausif Khan <khantausifkhan123@gmail.com>
2023-06-15 03:00:58 +0530

    asdsa

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'nunito': ['nunito', 'sans-serif'],
-        'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+        nunito: ["nunito", "sans-serif"],
+        MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
+      },
+      animation: {
+        move: "move 3s infinite linear",
+        rotate: "rotate 25s infinite linear",
+      },
+      keyframes: {
+        move: {
+          "0%": { transform: "rotate(0deg) translateX(40px) rotate(0deg)" },
+          "100%": {
+            transform: "rotate(360deg) translateX(40px) rotate(-360deg)",
+          },
+        },
+        rotate: {
+          "0%": {transform: "rotate(0deg)"},
+          "100%": {transform: "rotate(360deg)"}
+        }
       },
     },
   },
   plugins: [],
-}
-
+};
