def6d9e2c7ac0fbfea9d11aa2746cd4a302debe9
Tausif Khan <khantausifkhan123@gmail.com>
2023-06-18 17:37:50 +0530

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,9 +40,9 @@ module.exports = {
           },
         },
         rotate: {
-          "0%": {transform: "rotate(0deg)"},
-          "100%": {transform: "rotate(360deg)"}
-        }
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
